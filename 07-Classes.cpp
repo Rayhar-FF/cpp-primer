43d8a13fdd19f135db6a380d1976f4b2d027aaf2
Haris Faisal <harisfaisal47@gmail.com>
2025-12-26 17:21:10 +0500

    Classes Notes

---
+++ 07-Classes.cpp
@@ -0,0 +1,11 @@
+/*
+
+	# A class defines any object.
+
+	# The object being defined in a c++ class can have attributes and functionality coded into it.
+
+	# Classes can have their own data and variables. Addtional Functions for added functionality to class object operations through overloaded operators.
+
+	# Classes will be covered in greater detail later on which will include class composition and class features.
+
+*/
