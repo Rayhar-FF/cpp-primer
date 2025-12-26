9c1aaf50f3b9fe047a6cbb52c23eaecdb9064ee0
Haris Faisal <harisfaisal47@gmail.com>
2025-12-26 12:52:02 +0500

    if #3

--- 06-IfStatement.cpp
+++ 06-IfStatement.cpp
@@ -5,29 +5,28 @@
 int main()
 {
 	int currVal = 0, val = 0;          // *** currVal is the number being counted. New values are read in to val.
-	if (std::cin >> currVal)           // *** Input the first number to ensure there is data to process. 
-									   // ***Input to console current value(currVal).
+	if (std::cin >> currVal)           // *** Evaluate the condition currVal Check if data in currVal is valid. Do this to this input from the console.
+									   
 	{
-		int cnt = 1;				   // *** Store count for current value being processed.
+		int cnt = 1;				   // *** Store count for current value being processed, 1 bu default.
 		while (std::cin >> val)        // *** while inputting values , input to val.
 		{
-			if (val == currVal)		   // *** If values are the same add 1 to cnt.
+			if (val == currVal)		   // *** If values are the same add 1 to cnt. the == operator checks for equiality.
 			{
-				++cnt;
+				++cnt;				   // *** Increment count by 1(++) operator if val == currVal.
 			}
-			else					   // *** If the values are not same.
+			else					   // *** Otherwise or If the values are not same.
 									   
 			{
-				std::cout << currVal << " occurs " << cnt << " times" << std::endl; // *** Output to console the count of the value. 
-				currVal = val;														// *** CurrVal = val & add 1 to count.
-				cnt = 1;
-			}
+				std::cout << currVal << " occurs " << cnt << " times" << std::endl; // *** Output to console the count of currVal. 
+				currVal = val;														// *** CurrVal = val & add 1 to count. Reset
+				cnt = 1;															// *** Reset count.
+			}											
 			
 		}
-		std::cout << currVal << " occurs " << cnt << " times" << std::endl;		    // *** Output to console the count of the value. 
-	}
-	return 0;
+		std::cout << currVal << " occurs " << cnt << " times" << std::endl;		    // *** Output to console the count of the value.
+																					// ### Since the code in the book resets that values of currVal and cnt
+	}																				// ### In the else statement the program may somes overlook values.
+	return 0;																		// ### It should be obvious how to handle this.
 }
 
-// ####### The program above tries to calculate the number of times a number appears in a sequence but sometimes gives incorrect results.
-// ####### I will leave it in its current state.
