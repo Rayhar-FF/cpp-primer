826376c00aa1b0392617174f6fdc7eebef3e93c8
Haris Faisal <harisfaisal47@gmail.com>
2025-12-30 16:24:19 +0500

    Types

---
+++ 08-Types.cpp
@@ -0,0 +1,24 @@
+/**
+	Types: C++ is a statically typed language which means that the object's type is defined when you are writing the code as opposed to 
+	compile time(like python). 
+
+	So in pythong you can say.
+
+	my_num = 1
+
+	But in C++ you have to define the type first.
+
+	int my_num = 1;
+
+	this states that my_num is of type int and its value is initialized to 1.
+
+	Types determing the meaning of data and operations in our programs even of something simple such as  i = i + j.
+
+	See file 08-arithmetictypes to view all the arithmetic types. They can be further divided into signed and unsigned types
+
+	Signed types can have negative vallues while unsigned types can only be positive. Note that The third type is the type itself.
+	without signed or unsigned at the beginning.
+
+
+
+*/
